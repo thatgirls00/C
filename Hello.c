163f49f6bd5b664594906c464854ebe2943e0c09
thatgirls00 <wjsgustn201@gmail.com0>
2022-07-20 16:00:31 +0900

    첫번째 파일

---
+++ Hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //전처리기 문장. stdio.h 헤더파일을 전처리
+
+int main() //메인 함수 
+{
+    int result; //변수 선언문
+    result=100-50; //변수 초기화
+    result=80; //변수 대입문
+    printf("%d", result); //모니터에 출력하는 printf()함수를 호출
+
+    return 0; //반환문
+}
